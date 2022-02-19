f8491594799aa44fca3f54ac325607730a521e6c
Donal Cahill <magicmole@gmail.com>
2022-02-19 13:27:33 +0000

    fix path fillRule and clipRule

--- src/renderer/TitleBar/icons/Close.tsx
+++ src/renderer/TitleBar/icons/Close.tsx
@@ -10,8 +10,8 @@ export default function CloseIcon() {
       fill="currentColor"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
       />
     </svg>
