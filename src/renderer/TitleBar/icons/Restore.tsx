0d434ec3aa276e112e2827013440a86eae10ca12
Donal Cahill <magicmole@gmail.com>
2022-02-19 11:31:15 +0000

    update chakra-ui

--- src/renderer/TitleBar/icons/Restore.tsx
+++ src/renderer/TitleBar/icons/Restore.tsx
@@ -10,8 +10,8 @@ export default function RestoreIcon() {
     >
       <path d="M3 5v9h9V5H3zm8 8H4V6h7v7z" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5 5h1V4h7v7h-1v1h2V3H5v2z"
       />
     </svg>
