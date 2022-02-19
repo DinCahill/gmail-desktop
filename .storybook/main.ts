db3c54d014ad7dfee0c9966d856587945cddca41
Donal Cahill <magicmole@gmail.com>
2022-02-19 22:49:54 +0000

    update framer-motion; fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,3 @@
-const path = require('path')
-
-const toPath = (_path) => path.join(process.cwd(), _path)
-
 module.exports = {
   stories: ['../src/renderer/**/stories.tsx'],
   addons: [
@@ -12,17 +8,23 @@ module.exports = {
   typescript: {
     reactDocgen: false
   },
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react')
-        }
-      }
-    }
+  framework: '@storybook/react',
+  // unpins Storybook's dependence on Emotion 10 so that build can compile successfully
+  features: { emotionAlias: false },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // https://github.com/polkadot-js/extension/issues/621#issuecomment-759341776
+    // framer-motion uses the .mjs notation and we need to include it so that webpack will
+    // transpile it for us correctly (enables using a CJS module inside an ESM).
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+    // Return the altered config
+    return config
   }
 }
